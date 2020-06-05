6329b8ca6f5684e7d2d10209272013bdf4d85bec
t_fraser <david.rt.fraser@gmail.com>
2020-06-05 09:42:26 +0100

    db update

--- script-mod.py
+++ script-mod.py
@@ -4,14 +4,38 @@ from random import randint
 from random import shuffle
 import requests
 from time import sleep
+'''
+from fake_useragent import UserAgent
+import os
+import sqlite3
+import shutil
+from stem import Signal
+from stem.control import Controller
+import socket
+import socks
 
+controller = Controller.from_port(port=9051)
+controller.authenticate()'''
+
+req = requests.Session()
 base_url = 'https://www.ericjackson.com'
+'''
+def connectTor():
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5 , "127.0.0.1", 9050)
+    socket.socket = socks.socksocket
+
+def renew_tor():
+    controller.signal(Signal.NEWNYM)
+    
+UA = UserAgent(fallback='Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2')
+hdr = {'User-Agent': UA.random}'''
+hdr = {'User-Agent': 'Mozilla/5.0'}
 
 def get_html(url):
     
     html_content = ''
     try:
-        page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        page = req.get(url, headers=hdr)
         html_content = BeautifulSoup(page.content, "html.parser")
     except: 
         pass
@@ -69,7 +93,9 @@ def get_details(url):
         category = html.select('.BreadCrumb a')[1].get_text().strip()
         stamp['category'] = category
     except:
-        stamp['category'] = None  
+        stamp['category'] = None
+        
+    stamp['base_category'] = selection  
        
     try:
         subcategory = html.select('.BreadCrumb a')[2].get_text().strip()
@@ -160,6 +186,119 @@ def get_categories(url, element_class):
     shuffle(list(set(items)))
     
     return items
+'''
+def file_names(stamp):
+    file_name = []
+    rand_string = "RAND_"+str(randint(0,100000000))
+    file_name = [rand_string+"-" + str(i) + ".png" for i in range(len(stamp['image_urls']))]
+    print (file_name)
+    return(file_name)
+
+def query_for_previous(stamp):
+    # CHECKING IF Stamp IN DB
+    os.chdir("/Volumes/Stamps/")
+    conn1 = sqlite3.connect('Reference_data.db')
+    c = conn1.cursor()
+    col_nm = 'url'
+    col_nm2 = 'raw_text'
+    unique = stamp['url']
+    unique2 = stamp['raw_text']
+    c.execute('SELECT * FROM eric_jackson WHERE {cn} == "{un}" AND {cn2} == "{un2}"'.format(cn=col_nm, cn2=col_nm2, un=unique, un2=unique2))
+    all_rows = c.fetchall()
+    conn1.close()
+    price_update=[]
+    price_update.append((stamp['url'],
+    stamp['raw_text'],
+    stamp['scrape_date'], 
+    stamp['price'], 
+    stamp['currency']))
+    
+    if len(all_rows) > 0:
+        print ("This is in the database already")
+        conn1 = sqlite3.connect('Reference_data.db')
+        c = conn1.cursor()
+        c.executemany("""INSERT INTO price_list (url, raw_text, scrape_date, price, currency) VALUES(?,?,?,?,?)""", price_update)
+        try:
+            conn1.commit()
+            conn1.close()
+        except:
+            conn1.commit()
+            conn1.close()
+        print (" ")
+        sleep(randint(35,100))
+        next_step = 'continue'
+    else:
+        os.chdir("/Volumes/Stamps/")
+        conn2 = sqlite3.connect('Reference_data.db')
+        c2 = conn2.cursor()
+        c2.executemany("""INSERT INTO price_list (url, raw_text, scrape_date, price, currency) VALUES(?,?,?,?,?)""", price_update)
+        try:
+            conn2.commit()
+            conn2.close()
+        except:
+            conn2.commit()
+            conn2.close()
+        next_step = 'pass'
+    print("Price Updated")
+    return(next_step)
+
+def db_update_image_download(stamp): 
+    req = requests.Session()
+    directory = "/Volumes/Stamps/stamps/eric_jackson/" + str(datetime.datetime.today().strftime('%Y-%m-%d')) +"/"
+    image_paths = []
+    names = file_names(stamp)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+    image_paths = [directory + names[i] for i in range(len(names))]
+    for item in range(0,len(names)):
+        print (stamp['image_urls'][item])
+        try:
+            imgRequest1=req.get(stamp['image_urls'][item],headers=hdr, timeout=120, stream=True)
+        except:
+            print ("waiting...")
+            sleep(randint(3000,6000))
+            print ("...")
+            imgRequest1=req.get(stamp['image_urls'][item], headers=hdr, timeout=120, stream=True)
+        if imgRequest1.status_code==200:
+            with open(names[item],'wb') as localFile:
+                imgRequest1.raw.decode_content = True
+                shutil.copyfileobj(imgRequest1.raw, localFile)
+                sleep(randint(18,30))
+    stamp['image_paths']=", ".join(image_paths)
+    database_update =[]
+    # PUTTING NEW STAMPS IN DB
+    database_update.append((
+        stamp['url'],
+        stamp['raw_text'],
+        stamp['title'],
+        stamp['base_category'],
+        stamp['category'],
+        stamp['subcategory'],
+        stamp['condition'],
+        stamp['grade'],
+        stamp['scrape_date'],
+        stamp['image_paths']))
+    os.chdir("/Volumes/Stamps/")
+    conn = sqlite3.connect('Reference_data.db')
+    conn.text_factory = str
+    cur = conn.cursor()
+    cur.executemany("""INSERT INTO eric_jackson ('url','raw_text', 'title','base_category','category','subcategory',
+    'condition','grade','scrape_date','image_paths') 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", database_update)
+    try:
+        conn.commit()
+        conn.close()
+    except:
+        conn.commit()
+        conn.close()
+    print ("all updated")
+    print ("++++++++++++")
+    print (" ")
+    sleep(randint(55,160)) 
+
+connectTor()
+count = 0'''
 
 item_dict = {
 "United States": "https://www.ericjackson.com/rHome1d.asp?Header=USA&x=",
@@ -200,4 +339,29 @@ for category in categories:
         while(page_url):
             page_items, page_url = get_page_items(page_url)
             for page_item in page_items:
-                stamp = get_details(page_item) 
+                '''count += 1
+                if count > randint(30,156):
+                    print('Sleeping...')
+                    sleep(randint(1000, 4000))
+                    hdr['User-Agent'] = UA.random
+                    renew_tor()
+                    connectTor()
+                    count = 0
+                else:
+                    pass'''
+                stamp = get_details(page_item)
+                '''if stamp['price']==None or stamp['price']=='':
+                    sleep(randint(500,700))
+                    continue
+                next_step = query_for_previous(stamp)
+                if next_step == 'continue':
+                    print('Only updating price')
+                    continue
+                elif next_step == 'pass':
+                    print('Inserting the item')
+                    pass
+                else:
+                    break
+                db_update_image_download(stamp)
+                sleep(randint(10,30))'''
+print('Scrape Complete!')
